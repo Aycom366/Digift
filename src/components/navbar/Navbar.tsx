b2ff5ab3995738c522ee947b9e5b459d58d76c22
aycom366 <bamigboyeayomide200@gmail.com>
2022-06-06 15:34:44 +0100

    fixed banner image

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,19 +5,24 @@ import { FaBars } from "react-icons/fa";
 
 //utils
 import { useGlobalContext } from "~/store/Context";
+import { useNavigate } from "react-router-dom";
 
 //components
-import { ThemeToggler } from "./ThemeToggler";
 import Mobile from "./Mobile";
 import Desktop from "./Desktop";
 
 const Navbar = () => {
   const { isNavOpen, setIsNavOpen } = useGlobalContext();
+  const navigate = useNavigate();
   return (
     <nav className="bg-white dark:bg-slate-800 text-[#8A91A8] shadow-custom ">
       <div className="flex relative flex-col lg:flex-row w-full  px-4 py-4 max-w-[1400px]  mx-auto justify-between items-center">
         <div className="w-full  flex justify-between items-center">
-          <div role="link" className="w-[100px] h-[50px]">
+          <div
+            role="link"
+            onClick={() => navigate("/")}
+            className="w-[100px] h-[50px]"
+          >
             <img
               className="img object-contain"
               src={digiftLogo}
